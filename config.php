2fe85bd92b981cb50d90092f9adc52c50f7f029a
acctgjames5-ctrl <acctgjames5@gmail.com>
2026-08-31 11:25:24 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-$databaseUrl = getenv('postgresql://neondb_owner:npg_i0zfq7OoLTMQ@ep-frosty-surf-azo6it4t-pooler.c-3.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require');
+$databaseUrl = getenv('DATABASE_URL');
 
 if (!$databaseUrl) {
     die("DATABASE_URL is not configured.");
@@ -42,8 +42,8 @@ try {
 } catch (PDOException $e) {
 
     die(
-        "Database connection failed: "
-        . htmlspecialchars(
+        "Database connection failed: " .
+        htmlspecialchars(
             $e->getMessage(),
             ENT_QUOTES,
             'UTF-8'
